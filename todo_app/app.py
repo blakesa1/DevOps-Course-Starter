5465dc8a73a88204344065a3585bfeb1b4a5c729
blakesa1 <109984878+blakesa1@users.noreply.github.com>
2022-10-04 18:05:29 +0100

    index added

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,6 +5,7 @@ from flask import render_template
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
+import todo_app.data.session_items as session_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,5 +13,9 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+    items =session_items.get_items()
     return render_template("index.html", items = items)
+
+
+if __name__ == ' __main__':
+    app.run()
